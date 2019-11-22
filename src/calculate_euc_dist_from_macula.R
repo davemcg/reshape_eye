60405cb964b7fc2e8e3e57609866397be6ce3c9e
David McGaughey <mcgaughey@gmail.com>
2019-11-22 14:40:35 -0500

    two scripts

--- src/calculate_euc_dist_from_macula.R
+++ src/calculate_euc_dist_from_macula.R
@@ -3,11 +3,17 @@
 # second csv is entire eye
 # third value is the downsample 
 # fourth value is the new file name
+# if fifth value given, then outputs a pdf (named with fifth value) showing 
+# where the macula is calculated
+# example:
+# Rscript ~/git/reshape_eye/src/calculate_euc_dist_from_macula.R 18-0761-W-\(OD\)-48_P1F.csv 18-0761-W-\(OD\)/18-0761-W-\(OD\)-48_Data.csv 200000 18_0761_dist.Rdata test.pdf
+
 
 # calculates euclidean distance from all points in eye to the macula center
 args <- commandArgs(trailingOnly = TRUE)
 
 library(tidyverse)
+library(cowplot)
 macula <- readr::read_csv(args[1])
 full <- readr::read_csv(args[2])
 
@@ -22,3 +28,18 @@ downsample_distance <- full %>%
   mutate(Distance = euc.dist(macula_center, c(X,Y)))
 
 save(downsample_distance, file = args[4])
+
+if (!is.na(args[5])){
+  pdf(args[5], width = 7, height = 5)
+  print(
+    full %>% 
+      sample_n(50000) %>% 
+      ggplot(aes(x=X,y=-Y, color = log2(Area))) + 
+      geom_point(size = 0.3, alpha = 0.8) + 
+      geom_vline(aes(xintercept=macula_center[1])) + 
+      geom_hline(aes(yintercept = -macula_center[2])) + 
+      theme_cowplot() +
+      scale_color_viridis_c()
+  )
+  dev.off()
+}
--- src/plot_all.R
+++ src/plot_all.R
@@ -1,24 +1,34 @@
 # create smoothed distance plot 
 # x axis is euc. distance from macula
 # y axis is log2 scaled Area from ReShape
+# files are the Rdata distance files from `calculate_euc_dist_from_macula.R`
+# last input is the name of the plot
+# example: 
+# Rscript ~/git/reshape_eye/src/plot_all.R *Rdata dist_plot.pdf
 
+library(tidyverse)
+library(cowplot)
 
 args <- commandArgs(trailingOnly = TRUE)
 
-args <- c()
+files <- args
 
-load(files[1])
 all <- list()
-for (file in files){
+for (file in (files %>% rev())[2:length(files)]){
   load(file)
   all[[file]] <- downsample_distance
+  all[[file]]$Sample <- (str_split(file, '/')[[1]] %>% rev())[1] %>% gsub('_dist.Rdata', '', .)
 }
 
-all <- all %>% bind_rows(.id = 'File')
+all <- all %>% bind_rows()
 
-
-all %>% 
-  ggplot(aes(x=Distance, y = log2(Area), color = File)) + 
-  #geom_point(size = 0.1) + 
-  geom_smooth() + 
-  xlab('Euclidean Distance from Macula')
+pdf(file = (files %>% rev())[1], width = 6, height = 4)
+print(
+  all %>% 
+    ggplot(aes(x=Distance, y = log2(Area), color = Sample)) + 
+    #geom_point(size = 0.1) + 
+    geom_smooth() + 
+    xlab('Euclidean Distance from Macula') +
+    theme_cowplot()
+)
+dev.off()
